85c7c6cfb1ec5c0018d65a8b9312bf755fd685f5
siddontang <siddontang@gmail.com>
2012-02-26 22:14:12 +0800

    add auto-complete, yasnippet and python conf

---
+++ init-common.el
@@ -0,0 +1,7 @@
+(setq make-backup-files nil) 
+(global-font-lock-mode t)
+(transient-mark-mode t)
+
+(set-frame-font "-outline-Courier New-normal-normal-normal-mono-16-*-*-*-c-*-iso8859-1")
+
+(provide 'init-common)
---
+++ init.el
@@ -0,0 +1,10 @@
+;; -*- coding: utf-8 -*-
+(add-to-list 'load-path (expand-file-name "~/.emacs.d"))
+
+(require 'init-common)
+(require 'init-unicode)
+(require 'init-yasnippet)
+(require 'init-auto-complete)
+
+
+(require 'init-python-mode)
